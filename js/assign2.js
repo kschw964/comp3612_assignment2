dea832c6c21d6c95aa605ba8727877303f8f414e
kschw964 <kschw964@mtroyal.ca>
2023-11-20 15:33:17 -0700

    Implement JS to figure out "most popular" lists Implement JS to add entries to the respective <ul>

--- js/assign2.js
+++ js/assign2.js
@@ -7,22 +7,25 @@ const api =
    Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
    use built-in Live Preview.
 */
-const songs = JSON.parse(localStorage.getItem("songList"));
-if (!songs) {
-  fetch(api)
-    .then((response) => response.json())
-    .then((data) => {
-      localStorage.setItem("songList", JSON.stringify(data));
-      // call method that needs the songList using data as argument
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-} else {
-  // call method that needs the songList using the songs variable
-}
 
 document.addEventListener("DOMContentLoaded", () => {
+  const storedSongs = JSON.parse(localStorage.getItem("songList"));
+  if (!storedSongs) {
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem("songList", JSON.stringify(data));
+        // call methods that need the songList using data as argument
+        populateHomeScreen(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } else {
+    // call methods that need the songList using the storedSongs variable
+    populateHomeScreen(storedSongs);
+  }
+
   addPopupFunctionality();
   addNavClickListeners();
 });
@@ -61,4 +64,84 @@ function addNavClickListeners() {
     document.querySelector("main#playlist").classList.add("hidden");
     document.querySelector("main#song").classList.add("hidden");
   });
+}
+
+function populateHomeScreen(songList) {
+  populateGenreList(songList);
+  populateArtistList(songList);
+  populateSongList(songList);
+}
+
+function populateGenreList(songList) {
+  // figure out amount of songs that belong to a genre
+  const count = new Map();
+  for (const song of songList) {
+    if (count.has(song.genre.id)) {
+      count.set(song.genre.id, count.get(song.genre.id) + 1);
+    } else {
+      count.set(song.genre.id, 1);
+    }
+  }
+
+  // sort the map by converting it to an array
+  const sortedGenres = [...count].sort((one, other) => other[1] - one[1]);
+
+  const popularGenreUl = document.querySelector(
+    "main#home section:first-of-type ul"
+  );
+
+  for (let i = 0; i < 10; i++) {
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(
+        songList.find((element) => element.genre.id == sortedGenres[i][0]).genre
+          .name
+      )
+    );
+    popularGenreUl.appendChild(li);
+  }
+}
+
+function populateArtistList(songList) {
+  // figure out the number of songs an artists has
+  const count = new Map();
+  for (const song of songList) {
+    if (count.has(song.artist.id)) {
+      count.get(song.artist.id).count += 1;
+    } else {
+      count.set(song.artist.id, { name: song.artist.name, count: 1 });
+    }
+  }
+
+  // sort the map by converting it to an array
+  const sortedArtists = [...count.values()].sort(
+    (one, other) => other.count - one.count
+  );
+
+  const popularArtistsUl = document.querySelector(
+    "main#home section:nth-of-type(2) ul"
+  );
+
+  for (let i = 0; i < 10; i++) {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(sortedArtists[i].name));
+    popularArtistsUl.appendChild(li);
+  }
+}
+
+function populateSongList(songList) {
+  // sort songs by popularity
+  const sorted = songList.sort(
+    (one, other) => other.details.populatiry - one.details.populatiry
+  );
+
+  const popularSongsUl = document.querySelector(
+    "main#home section:last-of-type ul"
+  );
+
+  for (let i = 0; i < 10; i++) {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(sorted[i].title));
+    popularSongsUl.appendChild(li);
+  }
 }
